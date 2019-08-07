acf93b1e9f31bf2bc08d1883710e6249bcf7eb7f
Yu Ting Lai <kevin83825.ece05g@g2.nctu.edu.tw>
2019-08-07 09:41:50 +0800

    update source cpp

--- PSP/source.cpp
+++ PSP/source.cpp
@@ -31,9 +31,11 @@ int main(int argc, const char **argv)
 	if (owl.open(address) <= 0 || owl.initialize() <= 0)
 	{
 		cerr << "[Main] Cannot connect and initialize..." << endl;
-		cerr << "[Main] Check (1) Is the server operating? or (2) Is the IP address valid?" << endl;
+		cerr << "[Main] Check (1) Is the server operating? or ";
+		cerr << "(2) Is the IP address valid ? or (3) Is the cable connected ? " << endl;
 		return 0;
 	}
+	cout << "[Main] Connection established" << endl;
 
 	// create Ctrl+C handler, exit and save data upon pressing Ctrl+C
 	signal(SIGINT, ExitHandler);
@@ -41,6 +43,7 @@ int main(int argc, const char **argv)
 	// start streaming
 	owl.streaming(1);
 
+	int markerCount;
 	// main loop
 	while (owl.isOpen() && owl.property<int>("initialized") && !gFlag)
 	{
@@ -53,22 +56,25 @@ int main(int argc, const char **argv)
 		}
 		else if (event->type_id() == OWL::Type::FRAME)
 		{
-			cout << "time = " << event->time() << " " << event->type_name() << " ";
-			cout << event->name() << " = " << event->size<OWL::Event>() << ": " << endl;
+			//cout << "time = " << event->time() << " " << event->type_name() << " ";
+			//cout << event->name() << " = " << event->size<OWL::Event>() << ": " << endl;
 
 			if (event->find("markers", markers) > 0)
 			{
 				recordData.SetCurFrameNum(event->time()); // set the frame number for recording
 				cout << "markers = " << markers.size() << ": " << endl;
+				markerCount = 0;
 				for (OWL::Markers::iterator m = markers.begin(); m != markers.end(); m++)
 				{
 					if (m->cond > 0) // positive values are good condition values
 					{
 						recordData.SetCurData(m->id, m->x, m->y, m->z);
-						cout << " " << m->id << ") frame = " << m->time  << " "; 
-						cout << "x = "<< m->x << ", y = " << m->y << ", z = " << m->z << endl;
+						//cout << " " << m->id << ") frame = " << m->time  << " "; 
+						//cout << "x = "<< m->x << ", y = " << m->y << ", z = " << m->z << endl;
+						markerCount++;
 					}
 				} // end for
+				cout << "[Main] Find " << markerCount << " markers." << endl;
 				recordData.SetDataPerFrame();
 			} // end if find markers
 		}
