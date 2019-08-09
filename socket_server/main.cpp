481c29aa38e59d9c17c002c39799a2bb1119ef16
Kevin Lai <kevin83825.ece05g@g2.nctu.edu.tw>
2019-08-09 21:18:31 +0800

    remove ctrl-c handler

--- socket_server/main.cpp
+++ socket_server/main.cpp
@@ -1,16 +1,8 @@
 #include "socket_server.h"
 #include <signal.h>
 
-int gFlag = 0;
-
-void ExitHandler(int s)
-{
-	gFlag = 1;
-}
-
 int main(int argc, const char **argv)
 {
-	signal(SIGINT, ExitHandler);
 	SocketServer socketServer;
 	socketServer.CreateSocket();
 	socketServer.BindAndListen();
