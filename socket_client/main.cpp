6ea29bd88bfae8dc082a0cf5d9b72d96a9b9d20f
Yu Ting Lai <kevin83825.ece05g@g2.nctu.edu.tw>
2019-08-19 16:50:41 +0800

    add custom ip address from cin

--- socket_client/main.cpp
+++ socket_client/main.cpp
@@ -1,10 +1,18 @@
 #include "socket_client.h"
 #include <signal.h>
+#include <string>
 
 //#define OFFLINE
 
 int main(int argc, const char **argv)
 {
+	// read target IP address for transmission
+	std::string targetIP;
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(0);
+	std::cin >> targetIP;
+
+	// main code starts here
 	SocketClient socketClient;
 	socketClient.CreateSocket();
 	socketClient.ConnectToServer();
