90eae6c85fbef5a4c84d59af76d018e78941d78b
Yu Ting Lai <kevin83825.ece05g@g2.nctu.edu.tw>
2019-08-21 15:32:35 +0800

    remove socket sending wait time

--- PSP/source.cpp
+++ PSP/source.cpp
@@ -39,7 +39,7 @@ void SocketHostStreaming()
 	gCommunication.CreateSocket();
 	gCommunication.BindAndListen();
 	do {
-		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		//std::this_thread::sleep_for(std::chrono::milliseconds(1));
 		gCommunication.SendMsg();
 	} while (!gCommunication.flag);
 }
