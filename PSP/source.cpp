461fc2c0b10103be598eed8237f1a6e256b356e7
Yu Ting Lai <kevin83825.ece05g@g2.nctu.edu.tw>
2019-09-02 14:13:56 +0800

    code refactor

--- PSP/source.cpp
+++ PSP/source.cpp
@@ -11,7 +11,6 @@
 
 using namespace std;
 
-int gFlag = 0;
 Communication gCommunication;
 
 void ExitHandler(int s)
@@ -42,7 +41,6 @@ void SocketHostStreaming()
 	gCommunication.CreateSocket();
 	gCommunication.BindAndListen();
 	do {
-		//std::this_thread::sleep_for(std::chrono::milliseconds(1));
 		gCommunication.SendMsg();
 	} while (!gCommunication.flag);
 	return;
